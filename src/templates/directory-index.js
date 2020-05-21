7620d69c72453f102c0eab792b54d437e1c86911
roadlittledawn <clinton.langosch@gmail.com>
2020-05-20 17:50:36 -0700

    fix deployed build error for these imports

--- src/templates/directory-index.js
+++ src/templates/directory-index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
 
 export default (props) => {
   const displayName = props.pageContext.slug;
